4f3bcb719eeea8b695a9926c66886a68aa614729
martyntaylor <mtaylor@redhat.com>
2010-08-05 15:59:17 +0100

    Basic Rendered - Moved Value Markers Closer to Graph

--- lib/scruffy/renderers/basic.rb
+++ lib/scruffy/renderers/basic.rb
@@ -10,9 +10,9 @@ module Scruffy::Renderers
     def define_layout
       components << Scruffy::Components::Background.new(:background, :position => [0,0], :size =>[100, 100])
       components << Scruffy::Components::Viewport.new(:view, :position => [2, 26], :size => [89, 66]) do |graph|
-          graph << Scruffy::Components::Graphs.new(:graphs, :position => [3, 0], :size => [80, 89])
+          graph << Scruffy::Components::Graphs.new(:graphs, :position => [2, 0], :size => [80, 89])
           graph << Scruffy::Components::ValueMarkers.new(:values, :position => [0, 0], :size => [1, 89])
-          graph << Scruffy::Components::Axis.new(:values, :position => [3, 0], :size => [80, 89])
+          graph << Scruffy::Components::Axis.new(:values, :position => [2, 0], :size => [80, 89])
       end
       yield(self.components) if block_given?
     end
